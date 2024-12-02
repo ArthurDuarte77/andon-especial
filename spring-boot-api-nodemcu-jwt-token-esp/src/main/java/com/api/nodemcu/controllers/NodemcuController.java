260f87ee732ef3ed071fe2d47036890dcc17ad96
ArthurDuarte77 <bolivarartur77yt@gmail.com>
2024-12-02 08:39:59 -0300

    correção da api

--- spring-boot-api-nodemcu-jwt-token-esp/src/main/java/com/api/nodemcu/controllers/NodemcuController.java
+++ spring-boot-api-nodemcu-jwt-token-esp/src/main/java/com/api/nodemcu/controllers/NodemcuController.java
@@ -1,12 +1,13 @@
 package com.api.nodemcu.controllers;
-
+// veio do servidor
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import com.api.nodemcu.model.OperationModel;
 import com.api.nodemcu.repository.NodemcuRepository;
 import com.api.nodemcu.repository.OperationRepository;
@@ -14,15 +15,12 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import java.util.ArrayList;
 import jakarta.transaction.Transactional;
-import java.util.HashMap;
-import java.util.Map;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 @Controller
 public class NodemcuController {
+
     @Autowired
     private NodemcuRepository nodemcuRepository;
 
@@ -55,25 +53,22 @@ public class NodemcuController {
             messagingTemplate.convertAndSend(
                     "/user/" + operation.getName() + "/news",
                     status);
-        } catch (
-
-        InvalidDataAccessApiUsageException e) {
+        } catch (InvalidDataAccessApiUsageException e) {
             throw new RuntimeException(e);
         }
     }
 
-    public void enviarMensagemParaCliente(String op) {
-
+    @GetMapping("/send-news")
+    @ResponseBody
+    public void sendNews(@RequestParam String op, @RequestParam String status) {
         OperationModel operation = operationRepository.findByName(op);
-        if (operation != null) {
-            Object resultado = nodemcuRepository.findByNameId(operation);
-            if (resultado != null) {
-                messagingTemplate.convertAndSend("/topic/news", resultado);
-            } else {
-                System.out.println("Aviso: Resultado nulo ao buscar por NameId");
-            }
-        } else {
-            System.out.println("Aviso: Operação não encontrada");
+        try {
+            nodemcuRepository.updateStateByNameId(status, operation.getId());
+            messagingTemplate.convertAndSend(
+                    "/user/" + operation.getName() + "/news",
+                    status);
+        } catch (InvalidDataAccessApiUsageException e) {
+            throw new RuntimeException(e);
         }
     }
 }
